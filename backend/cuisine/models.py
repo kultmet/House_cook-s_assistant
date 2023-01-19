9264a4f456af474264f956b7a51734d899b0e91c
Maystrenko Dmitriy <kultmet@gmail.com>
2023-01-19 18:07:27 +0300

    Создал все модели для приложения cuisine

--- backend/cuisine/models.py
+++ backend/cuisine/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+
 class Recipe(models.Model):
+    """Модель для Рецепта."""
     name = models.CharField(
         max_length=200,
         verbose_name='Название рецепта'
@@ -35,3 +37,61 @@ class Recipe(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    """Модель для Тегов."""
+    name = models.CharField(
+        max_length=30,
+        verbose_name='Название Тега'
+    )
+    color = models.CharField(
+        max_length=15,
+        verbose_name='Цвет Тега'
+    )
+    slug = models.SlugField(
+        max_length=45,
+        verbose_name='Slug'
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=100
+    )
+    amount = models.IntegerField(
+        verbose_name='Колличество',
+        default=None
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Еденици измерения',
+        max_length=30
+    )
+
+class Favorite(models.Model):
+    """Модель для Избранного."""
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        related_name='favorites',
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        related_name='favorites',
+        on_delete=models.CASCADE
+    )
+
+
+class Order(models.Model):
+    """Модель для Покупки."""
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        related_name='orders',
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        related_name='orders'
+    )
