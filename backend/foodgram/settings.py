98d6fb6b7ef40af7f5c975d83c76248d61afec5d
Maystrenko Dmitriy <kultmet@gmail.com>
2023-01-23 13:21:44 +0300

    Сдалал модели Пользователя и подписки

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'djoser',
     'cuisine',
     'users',
     'api',
@@ -101,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 
 # Internationalization
